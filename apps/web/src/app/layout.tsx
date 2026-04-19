0ae8b17d1caf5d1a303a0a3d160d5958d916acc4
Whisker <witcoinwhisker@gmail.Com>
2026-04-19 17:08:37 +0200

    Revert "feat: add traaaction tracking script in root layout head"
    This reverts commit f38248926d6add90c0d57c8358f1155bc81cd3a3.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -31,13 +31,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className={jakarta.variable} suppressHydrationWarning>
-      <head>
-        <script
-          defer
-          src="https://track.traaaction.com/trac.js"
-          data-key="pk_OIe8Q70sI3QqNYAMa4Xun2um"
-        />
-      </head>
       <body className="font-sans">
         <script
           type="application/ld+json"
